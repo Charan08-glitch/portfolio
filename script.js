9f048f923d37081105ac051758592476acf1bb3f
Charan08-glitch <tejavathcharan08@gmail.com>
2025-05-14 22:35:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click", function() {
+  window.location.href = "https://wa.me/8500567238"; 
+});
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.querySelector(".navbar-links").classList.toggle("open");
+});
