38b9b87c1e5daf67b9696d5c37365ba7e567ed12
Charan08-glitch <tejavathcharan08@gmail.com>
2025-05-14 22:56:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("btn").addEventListener("click", function() {
-  window.location.href = "https://wa.me/8500567238"; 
+  window.location.href = "https://wa.me/918500567238"; 
 });
 document.getElementById("menu-toggle").addEventListener("click", function() {
     document.querySelector(".navbar-links").classList.toggle("open");
